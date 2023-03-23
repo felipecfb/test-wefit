c3228971d56c6a582bd3aba980349982268d6a39
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-03-23 17:41:22 -0300

    💄 styles: creating global styles

---
+++ src/styles/index.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+  }
+  :root {
+    --white: #fff;
+
+    --background: #2F2E41;
+
+    --gray: #333333;
+
+    --blue: #009EDD;
+
+    --green: #039B00;
+  }
+  
+  @media screen and (max-width: 1080px) {
+      html {
+          font-size: 93.75%;
+      }
+  }
+  @media screen and (max-width: 720px) {
+      html {
+          font-size: 87.5%;
+      }
+  }
+  body {
+      background: var(--background);
+      color: var(--white);
+  }
+  body, input, select, textarea, button {
+      font: 400 1rem "Open Sans", sans-serif;
+  }
+  button {
+      cursor: pointer;
+  }
+  a {
+      color: inherit;
+      text-decoration: none;
+  }
+`;
