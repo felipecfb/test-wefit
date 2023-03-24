4154e014481b354888d57f0cecd1347b337c9e2a
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-03-24 12:03:33 -0300

    🚚 chore: rename the file to the global styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
-import { GlobalStyles } from './styles';
+import { GlobalStyles } from './styles/global';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
