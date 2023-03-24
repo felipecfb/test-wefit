94d993153e0c07e036dba3daaa0d88ba2afafa94
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-03-23 22:05:02 -0300

    ✨ feat: creating footer for finish order and total

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,11 +1,19 @@
+import { Button } from '../../components/Button';
 import { TableProducts } from './components/TableProducts';
-import { Container, Divider } from './styles';
+import { Container, Divider, Footer, Total, TotalWrap } from './styles';
 
 export function Cart() {
   return (
     <Container>
       <TableProducts />
       <Divider />
+      <Footer>
+        <Button title="Finalizar pedido" />
+        <TotalWrap>
+          <span>Total</span>
+          <Total>R$ 9,99</Total>
+        </TotalWrap>
+      </Footer>
     </Container>
   );
 }
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -16,3 +16,33 @@ export const Divider = styled.div`
   height: 1px;
   background: var(--gray-500);
 `;
+
+export const Footer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  button {
+    max-width: 235px;
+  }
+`;
+
+export const TotalWrap = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+
+  span {
+    font-size: 0.875rem;
+    color: var(--gray-500);
+    text-transform: uppercase;
+    font-weight: 700;
+  }
+`;
+
+export const Total = styled.p`
+  font-size: 1.5rem;
+  color: var(--gray-900);
+  font-weight: 700;
+`;
