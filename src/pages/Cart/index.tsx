ef33910becc04285921c7283661592428c6c4588
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-03-23 21:56:00 -0300

    ✨ feat: create component divider for cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import { EmptyCart } from './components/EmptyCart';
 import { TableProducts } from './components/TableProducts';
-import { Container } from './styles';
+import { Container, Divider } from './styles';
 
 export function Cart() {
   return (
     <Container>
       <TableProducts />
+      <Divider />
     </Container>
   );
 }
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -5,4 +5,14 @@ export const Container = styled.div`
   background: var(--white);
   border-radius: 4px;
   padding: 2rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 21px;
+`;
+
+export const Divider = styled.div`
+  width: 100%;
+  height: 1px;
+  background: var(--gray-500);
 `;
