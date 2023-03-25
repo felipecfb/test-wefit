b8d54506097af45d0e60a73011e7ee0f0091f7d7
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-03-25 12:15:05 -0300

    ✨ feat: correcting the header for the proposed design

--- wefit-test-javascript/js/main.js
+++ wefit-test-javascript/js/main.js
@@ -7,5 +7,20 @@ rows.forEach(row => rowsArray.push(row));
 rowsArray.forEach(row => {
   const title = row.querySelector('h3').textContent;
 
-  console.log(title);
+  if (title === 'Menu') {
+    const btnGroup = row.querySelector('div > .btn-group-vertical');
+    const buttons = btnGroup.querySelectorAll('button');
+
+    [...buttons].forEach(button => {
+      button.style.borderRadius = '4px';
+    });
+
+    const styles = {
+      'flex-direction': 'row',
+      'gap': '5px',
+      'white-space': 'nowrap',
+    };
+
+    Object.assign(btnGroup.style, styles);
+  };
 });
